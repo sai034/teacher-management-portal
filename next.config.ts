07e3da7811b69af743848a46d89c91a40b540dc5
sai034 <saisourabhcherukuru@gmail.com>
2025-07-12 00:06:33 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/teacher-management-portal',
+  assetPrefix: '/teacher-management-portal',
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+
 };
+
 
 export default nextConfig;
